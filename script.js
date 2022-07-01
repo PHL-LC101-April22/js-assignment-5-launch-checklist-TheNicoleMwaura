642776705b8d89273e07e1a28a646ae8794a74cf
TheNicoleMwaura <nikkikanyeri@gmail.com>
2022-07-01 15:16:24 -0400

    Working on the json

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ window.addEventListener("load", function() {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        
-        let planet = pickPlanet(listedPlanets);
+        let planet = pickPlanet(result);
         addDestinationInfo(document, planet.name, planet.diameter, planet.star, 
          planet.distance, planet.moon, planet.image);
         
